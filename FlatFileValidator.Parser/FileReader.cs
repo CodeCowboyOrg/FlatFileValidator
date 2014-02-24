2a273777aa0e365b7e993cf1a1018848aa543a59
CodeCowBoyOrg <CodeCowBoyOrg@gmail.com>
2014-02-24 00:16:14 -0500

    Initial Write.

---
+++ FlatFileValidator.Parser/FileReader.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace FlatFileValidator.Parser
+{
+    public class FileReader
+    {
+        private string m_fileName = null;
+        private int m_lineCount = 0;
+
+        #region Properties
+        public int LineCount
+        {
+            get { return m_lineCount; }
+            set { m_lineCount = value; }
+        }
+
+        public string FileName
+        {
+            get { return m_fileName; }
+            set { m_fileName = value; }
+        }
+        #endregion Properties
+        
+        #region Public Constructor
+        public FileReader(string fileName)
+        {
+            m_fileName = fileName;
+        }
+        #endregion Public Constructor
+
+        #region Public Methods
+
+        public void Process()
+        {
+            string path = m_fileName;
+            try
+            {
+                if (File.Exists(path))
+                {
+                    using (StreamReader sr = new StreamReader(path))
+                    {
+                        string line = null;
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            m_lineCount++;
+                        }
+
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The process failed: {0}", e.ToString());
+            }
+        }
+        #endregion Public Methods
+
+    }
+}
---
+++ FlatFileValidator/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using FlatFileValidator.Parser;
+
+namespace FlatFileValidator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FileReader fr = new FileReader(@"C:\Users\Johnny\Desktop\GEONAMES2013\allCountries\allCountries.txt");
+            fr.Process();
+            MessageBox.Show(fr.LineCount.ToString());
+
+        }
+    }
+}
